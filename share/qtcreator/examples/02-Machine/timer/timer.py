a67428ece39ed2d206bc05fff2254a81a63b4a53
huangziyi <huangziyi@canaan-creative.com>
2024-03-15 15:57:22 +0800

    update resource
    Signed-off-by: huangziyi <huangziyi@canaan-creative.com>

--- share/qtcreator/examples/02-Machine/timer/timer.py
+++ share/qtcreator/examples/02-Machine/timer/timer.py
@@ -1,10 +1,13 @@
-# 基础示例
-#
-# 欢迎使用CanMV IDE, 点击IDE左下角的绿色按钮开始执行脚本
-
 from machine import Timer
-def on_timer(arg):                                                                                          #定义定时器回调函数
-    print("time up: %d" % arg)
+import time
 
-tim = Timer(mode=Timer.MODE_ONE_SHOT,period=3, unit=Timer.UNIT_S, callback=on_timer, arg=1, start=True)     #构造定时器对象，时间为3s，默认开启定时器
-
+# 实例化一个软定时器
+tim = Timer(-1)
+# 初始化定时器为单次模式，周期100ms
+tim.init(period=100, mode=Timer.ONE_SHOT, callback=lambda t:print(1))
+time.sleep(0.2)
+# 初始化定时器为周期模式，频率为1Hz
+tim.init(freq=1, mode=Timer.PERIODIC, callback=lambda t:print(2))
+time.sleep(2)
+# 释放定时器资源
+tim.deinit()
